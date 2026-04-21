fa221b9bac51e6548a891bb302b00afa3209453a
Tebogo <notorioustee11@gmail.com>
2026-04-21 23:17:37 +0200

    Added Encryption for UserService.java

--- src/main/java/za/ac/cput/unitrade/service/UserService.java
+++ src/main/java/za/ac/cput/unitrade/service/UserService.java
@@ -1,5 +1,6 @@
 package za.ac.cput.unitrade.service;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import za.ac.cput.unitrade.dto.UserDTO;
@@ -13,6 +14,9 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
     public User register(UserDTO userDTO) {
         if (userRepository.existsByUniversityEmail(userDTO.getUniversityEmail())) {
             throw new RuntimeException("An account with this email already exists");
@@ -22,7 +26,7 @@ public class UserService {
         user.setFirstName(userDTO.getFirstName());
         user.setLastName(userDTO.getLastName());
         user.setUniversityEmail(userDTO.getUniversityEmail());
-        user.setPassword(userDTO.getPassword());
+        user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
 
         return userRepository.save(user);
     }
@@ -30,7 +34,7 @@ public class UserService {
     public Optional<User> login(String universityEmail, String password) {
         Optional<User> user = userRepository.findByUniversityEmail(universityEmail);
 
-        if (user.isPresent() && user.get().getPassword().equals(password)) {
+        if (user.isPresent() && passwordEncoder.matches(password, user.get().getPassword())) {
             return user;
         }
 
